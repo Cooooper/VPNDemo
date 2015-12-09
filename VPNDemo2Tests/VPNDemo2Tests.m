588a2c0a1b4a03fb0d713f6db5395a9d72db4628
Han Yahui <ydhan@ideabinder.com>
2015-12-09 14:29:16 +0800

    first commit

---
+++ VPNDemo2Tests/VPNDemo2Tests.m
@@ -0,0 +1,39 @@
+//
+//  VPNDemo2Tests.m
+//  VPNDemo2Tests
+//
+//  Created by Han Yahui on 15/12/7.
+//  Copyright © 2015年 Han Yahui. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VPNDemo2Tests : XCTestCase
+
+@end
+
+@implementation VPNDemo2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
